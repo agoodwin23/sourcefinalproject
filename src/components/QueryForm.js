830337e90a24258cb4ae21326d7910864a69d003
Abby Goodwin <abbygoodwin@Abbys-MacBook-Pro.local>
2020-11-15 12:16:54 -0500

    added queries

--- src/components/QueryForm.js
+++ src/components/QueryForm.js
@@ -1,25 +1,78 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Card } from 'react-bootstrap';
 
 function QueryForm(props) {
     const [formData, setFormData] = useState('');
 
     return (
         <div>
-            <Form onSubmit={e => {
-                e.preventDefault(); // prevents the screen from rendering, which loses form data entered
-                props.onSubmit(formData);
-            }}> 
-            <Form.Label>
-                Danceability:
-            </Form.Label>
-            <Form.Control type="text" placeholder="Enter a number between 0-1"
-                onChange={(e) => {
-                    let oldFormVals = { ...formData, danceability: e.target.value };
-                    setFormData(oldFormVals);
-            }} />
-            <Button type="submit" onClick={() => console.log(formData)}>Submit</Button>
-        </Form>
+            <Card body border="secondary" style={{ margin: '10px' }}>
+                <Form onSubmit={e => {
+                    e.preventDefault(); // prevents the screen from rendering, which loses form data entered
+                    props.onSubmit(formData);
+                }}> 
+                <Form.Label>
+                    Danceability:
+                </Form.Label>
+                <Form.Control type="text" placeholder="Enter a number between 0.0 and 1.0"
+                    onChange={(e) => {
+                        let oldFormVals = { ...formData, danceability: e.target.value };
+                        setFormData(oldFormVals);
+                }} />
+                <Button variant="success" onClick={() => console.log(formData)} style={{margin:"10px"}}>Submit</Button>
+            </Form>
+         </Card>
+
+            <Card body border="secondary" style={{ margin: '10px' }}>
+                <Form onSubmit={e => {
+                    e.preventDefault(); // prevents the screen from rendering, which loses form data entered
+                    props.onSubmit(formData);
+                }}> 
+                <Form.Label>
+                    Energy:
+                </Form.Label>
+                <Form.Control type="text" placeholder="Enter a number between 0.0 and 1.0"
+                    onChange={(e) => {
+                        let oldFormVals = { ...formData, energy: e.target.value };
+                        setFormData(oldFormVals);
+                }} />
+                <Button variant="success" onClick={() => console.log(formData)} style={{margin:"10px"}}>Submit</Button>
+            </Form>
+        </Card>
+
+            <Card body border="secondary" style={{ margin: '10px' }}>
+                <Form onSubmit={e => {
+                    e.preventDefault(); // prevents the screen from rendering, which loses form data entered
+                    props.onSubmit(formData);
+                }}> 
+                <Form.Label>
+                    Loudness:
+                </Form.Label>
+                <Form.Control type="text" placeholder="Enter a number between -60 and 0 decibels"
+                    onChange={(e) => {
+                        let oldFormVals = { ...formData, loudness: e.target.value };
+                        setFormData(oldFormVals);
+                }} />
+                <Button variant="success" onClick={() => console.log(formData)} style={{margin:"10px"}}>Submit</Button>
+            </Form>
+        </Card>
+
+             <Card body border="secondary" style={{ margin: '10px' }}>
+                <Form onSubmit={e => {
+                    e.preventDefault(); // prevents the screen from rendering, which loses form data entered
+                    props.onSubmit(formData);
+                }}> 
+                <Form.Label>
+                    Popularity:
+                </Form.Label>
+                <Form.Control type="text" placeholder="Enter a number between 0 and 100"
+                    onChange={(e) => {
+                        let oldFormVals = { ...formData, popularity: e.target.value };
+                        setFormData(oldFormVals);
+                }} />
+                <Button variant="success" onClick={() => console.log(formData)} style={{margin:"10px"}}>Submit</Button>
+            </Form>
+        </Card>
         </div>
     )
 }
